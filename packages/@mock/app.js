f529022cce069ce5c166d422850116b17db58ec0
chenjian <277527478@qq.com>
2019-08-16 09:40:28 +0800

    feat:新增cors 与 bodyParser

--- packages/@mock/app.js
+++ packages/@mock/app.js
@@ -1,11 +1,17 @@
 const Koa = require('koa')
 const chalk = require('chalk')
 const logger = require('koa-logger')
+const cors = require('koa2-cors')
+const bodyParser = require('koa-bodyparser')
 const Router = require('./routers')
 const log = console.log
 const app = new Koa()
+// 跨域 支持所有域名访问
+app.use(cors())
 // 日志
 app.use(logger())
+// 解析post body
+app.use(bodyParser())
 app.use(Router.routes())
 
 app.listen(3000)
